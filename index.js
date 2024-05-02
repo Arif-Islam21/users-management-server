fce2330982969b3d91c8cbf41243dda47be78774
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-02 22:41:11 +0600

    startwed my journey with backend

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
   console.log("Post api hitting");
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
 });
 
 app.get("/", (req, res) => {
