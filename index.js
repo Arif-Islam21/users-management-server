11bdaf214653d1df658f5f0841efb715f47504d4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-02 22:18:44 +0600

    express.json() middlewere

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middlewere
 app.use(cors());
+app.use(express.json());
 
 const users = [
   {
--- steps.js
+++ steps.js
@@ -1,4 +1,11 @@
 /**
  * 1. create a post api on the server side
  * 2. from client side , send data via post
+ * 3. set fetch information
+ * 4. set fetch method inside the fetch options
+ * 5. options will have three things : method
+ * 6. options will have headers: 'content-type' :'application/json'
+ * 7. body: dont forget to send data by JSON.stringify
+ * 8. on the server side dont forget to use express.json() midlewire
+ * 7.
  */
